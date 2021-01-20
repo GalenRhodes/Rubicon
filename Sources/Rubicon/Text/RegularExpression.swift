eddadd1cfa16211dfc72e1c59ef6852ca19a32fb
Galen Rhodes <galenrhodes@me.com>
2021-01-20 11:48:14 -0500

    Bug fixes

--- Sources/Rubicon/Text/RegularExpression.swift
+++ Sources/Rubicon/Text/RegularExpression.swift
@@ -27,7 +27,7 @@ open class RegularExpression {
 
     private let nsRegex: NSRegularExpression
 
-    public init(pattern: String, options: NSRegularExpression.Options) throws {
+    public init(pattern: String, options: NSRegularExpression.Options = []) throws {
         nsRegex = try NSRegularExpression(pattern: pattern, options: options)
     }
 
@@ -55,66 +55,69 @@ open class RegularExpression {
         public typealias Index = Int
 
         public let string:     String
-        public let startIndex: Index = 0
-        public let endIndex:   Index
+        public var startIndex: Index { groups.startIndex }
+        public var endIndex:   Index { groups.endIndex }
+        public var count:      Int { groups.count }
 
-        @usableFromInline let nsMatch: NSTextCheckingResult
+        @usableFromInline let nsMatch:    NSTextCheckingResult
+        @usableFromInline var namedCache: [String:NamedGroup] = [:]
+
+        @usableFromInline lazy var groups: [Group] = getGroups()
 
         public init(_ str: String, match: NSTextCheckingResult) {
             self.nsMatch = match
             self.string = str
-            self.endIndex = match.numberOfRanges
         }
 
-        @inlinable public subscript(name: String) -> Group {
-            let range = nsMatch.range(withName: name)
-            return Group(string, range: range)
+        @inlinable func getGroups() -> [Group] {
+            var grps: [Group] = []
+            for x in (0 ..< nsMatch.numberOfRanges) { grps.append(Group(self, range: nsMatch.range(at: x))) }
+            return grps
         }
 
-        @inlinable public subscript(index: Int) -> Group {
-            guard index >= 0 && index < count else { fatalError("Index out of range.") }
-            return Group(string, range: nsMatch.range(at: index))
+        @inlinable public subscript(name: String) -> NamedGroup {
+            if let ng = namedCache[name] { return ng }
+            let ng = NamedGroup(self, name: name, range: nsMatch.range(withName: name))
+            namedCache[name] = ng
+            return ng
         }
 
-        @inlinable public func makeIterator() -> Iterator { Iterator(match: self) }
+        public subscript(position: Index) -> Element { groups[position] }
 
-        public class Iterator: IteratorProtocol {
-            private var index: Int = 0
-            private let match: Match
+        public func index(after i: Index) -> Index { groups.index(after: i) }
+
+        public func makeIterator() -> Iterator { Iterator(match: self) }
+
+        public final class Iterator: IteratorProtocol {
+            public typealias Element = Group
+
+            @usableFromInline var index: Int = 0
+            @usableFromInline let match: Match
 
             public init(match: Match) { self.match = match }
 
-            public func next() -> Group? {
-                guard index < match.count else { return nil }
-                return match[index++]
-            }
-
-            public typealias Element = Group
-        }
-
-        @inlinable public func index(after i: Index) -> Index {
-            guard i >= 0 && i < count else { fatalError("Index out of range.") }
-            return (i + 1)
+            @inlinable public func next() -> Element? { (index < match.groups.endIndex ? match.groups[index++] : nil) }
         }
     }
 
-    open class Group {
-        private let str:   String
-        public let  range: Range<String.Index>?
-        public internal(set) lazy var subString: String? = ((range == nil) ? nil : String(str[range!]))
+    public class Group {
+        let match: Match
 
-        public init(_ str: String, range: NSRange) {
-            self.str = str
-            self.range = ((range.location == NSNotFound) ? nil : (String.Index(utf16Offset: range.lowerBound, in: str) ..< String.Index(utf16Offset: range.upperBound, in: str)))
+        public let range: Range<String.Index>?
+        public internal(set) lazy var subString: String? = ((range == nil) ? nil : String(match.string[range!]))
+
+        public init(_ match: Match, range: NSRange) {
+            self.match = match
+            self.range = ((range.location == NSNotFound) ? nil : (String.Index(utf16Offset: range.lowerBound, in: match.string) ..< String.Index(utf16Offset: range.upperBound, in: match.string)))
         }
     }
 
-    open class NamedGroup: Group {
+    public class NamedGroup: Group {
         public let name: String
 
-        public init(_ str: String, name: String, range: NSRange) {
+        public init(_ match: Match, name: String, range: NSRange) {
             self.name = name
-            super.init(str, range: range)
+            super.init(match, range: range)
         }
     }
 }
@@ -132,7 +135,7 @@ public typealias RegExResult = NSTextCheckingResult
 /// Convienience function to build an instance of <code>[RegEx](https://developer.apple.com/documentation/foundation/nsregularexpression/)</code> that includes the option to have
 /// anchors ('^' and '$') match the beginning and end of lines instead of the entire input.
 ///
-/// - Parameter patthen: the regular expression pattern.
+/// - Parameter pattern: the regular expression pattern.
 /// - Returns: the instance of <code>[RegEx](https://developer.apple.com/documentation/foundation/nsregularexpression/)</code>
 /// - Throws: exception if the pattern is an invalid regular expression pattern.
 ///

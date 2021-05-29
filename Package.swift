12c0d8a777af29d6468c4f847332c5ddc41a9fbf
Galen Rhodes <galenrhodes@me.com>
2021-05-28 20:10:18 -0400

    Bah!

--- Package.swift
+++ Package.swift
@@ -4,19 +4,36 @@
 import PackageDescription
 
 //@f:0
-let package = Package(
-  name: "Rubicon",
-  platforms: [ .macOS(.v10_15), .tvOS(.v13), .iOS(.v13), .watchOS(.v6) ],
-  products: [
-      .library(name: "Rubicon", type: .dynamic, targets: [ "Rubicon" ]),
-  ],
-  dependencies: [
-      .package(name: "RingBuffer", url: "https://github.com/GalenRhodes/RingBuffer", .upToNextMajor(from: "1.0.8")),
-  ],
-  targets: [
-      .systemLibrary(name: "iconv", providers: [ .apt([ "libc6-dev" ]) ]),
-      .target(name: "Rubicon", dependencies: [ "RingBuffer", "iconv" ], exclude: [ "Info.plist" ]),
-      .testTarget(name: "RubiconTests", dependencies: [ "Rubicon" ], exclude: [ "Info.plist" ], resources: [ .copy("Files") ]),
-  ]
-)
+#if os(macOS) || os(tvOS) || os(watchOS) || os(iOS)
+    let package = Package(
+      name: "Rubicon",
+      platforms: [ .macOS(.v10_15), .tvOS(.v13), .iOS(.v13), .watchOS(.v6) ],
+      products: [
+          .library(name: "Rubicon", type: .dynamic, targets: [ "Rubicon" ]),
+      ],
+      dependencies: [
+          .package(name: "RingBuffer", url: "https://github.com/GalenRhodes/RingBuffer", .upToNextMajor(from: "1.0.8")),
+      ],
+      targets: [
+          .systemLibrary(name: "iconv", providers: []),
+          .target(name: "Rubicon", dependencies: [ "RingBuffer", "iconv" ], exclude: [ "Info.plist" ]),
+          .testTarget(name: "RubiconTests", dependencies: [ "Rubicon" ], exclude: [ "Info.plist" ], resources: [ .copy("Files") ]),
+      ]
+    )
+#else
+    let package = Package(
+      name: "Rubicon",
+      platforms: [ .macOS(.v10_15), .tvOS(.v13), .iOS(.v13), .watchOS(.v6) ],
+      products: [
+          .library(name: "Rubicon", type: .dynamic, targets: [ "Rubicon" ]),
+      ],
+      dependencies: [
+          .package(name: "RingBuffer", url: "https://github.com/GalenRhodes/RingBuffer", .upToNextMajor(from: "1.0.8")),
+      ],
+      targets: [
+          .target(name: "Rubicon", dependencies: [ "RingBuffer" ], exclude: [ "Info.plist" ]),
+          .testTarget(name: "RubiconTests", dependencies: [ "Rubicon" ], exclude: [ "Info.plist" ], resources: [ .copy("Files") ]),
+      ]
+    )
+#endif
 //@f:1

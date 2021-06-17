27bc8cd1b0c05154901b7178740c7d30ef74b400
Galen Rhodes <galenrhodes@me.com>
2021-06-16 21:10:36 -0400

    Linux Tests

--- Sources/Rubicon/Streams/Character/SimpleIConvCharInputStream.swift
+++ Sources/Rubicon/Streams/Character/SimpleIConvCharInputStream.swift
@@ -248,7 +248,7 @@ internal let MAX_READ_AHEAD:      Int       = 65_536
                     print("Opening Input Stream...")
                     inputStream.open()
                     while inputStream.streamStatus == .opening {}
-                    print("Input Stream Opened...")
+                    print("Input Stream Opened...: \(inputStream.className)")
                     if let e = inputStream.streamError {
                         print("...with error: \(e)")
                         error = e
